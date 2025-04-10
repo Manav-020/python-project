8ba07e5ea04c24bd28043b2893c1dbf2af12df31
Neel Lapsiwala <neellapsiwala@gmail.com>
2025-04-10 12:16:20 +0530

    Update views.py

--- lims_project/lims_app/views.py
+++ lims_project/lims_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+
+#Register your models here.
+
 
 # Create your views here.
+def home(request):
+    return render(request,"home.html",context={"current_tab": "home"})
+
+def readers(request):
+    return render(request,"readers.html",context={"current_tab": "readers"})
+
+def shopping(request):
+    return HttpResponse("Welcome to shopping")
+
+def save_student(request):
+    student_name= request.POST['student_name']
+    return render(request,"welcome.html",context={'student_name':student_name})
